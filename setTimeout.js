e7bf2ef976813d25a7e1ded786056534d4edd003
krishpatel789 <krishpateltulisagari123@gmail.com>
2024-08-20 13:41:24 +0530

    Added SetTimeOut Concept

---
+++ setTimeout.js
@@ -0,0 +1,34 @@
+function x()
+{
+    let i=1;  
+    setTimeout(function(){console.log(i);},3000);
+    console.log("Check this Line are you there");
+    
+}
+x();
+
+
+
+
+
+
+
+
+
+/*
+function x()
+{
+    let i=1;
+    for(let k=0;k<10;k++)
+    {
+        setTimeout(function(){console.log(i);},3000);
+        console.log("In Middle");
+        i++;
+        
+    }
+    
+}
+x();
+console.log("FINALLY I CAME HERE");
+
+*/
